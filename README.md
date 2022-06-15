9c20688a4492ec7ecdc1c34f792703732338edc5
guillermo haas-thompson <guillermo@brownbox.me>
2022-06-15 14:52:35 -0400

    Update Readme
    Make code example generic. Add ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pixels
 A repo for our pixels. All the pixels.
+
+
+#### Notes
+
++ `hook_key` The account and webhook id of the Zapier webhook
++ `bid_no` The `bid_no` is used to retrieve info from the KELOH site
--- tenstreet/keloh.js
+++ tenstreet/keloh.js
@@ -1,4 +1,4 @@
 
 let params = (new URL(document.location)).searchParams;
 let bid_no = params.get("aqi_bid");
-bid_no && fetch(`https://hooks.zapier.com/hooks/catch/8923119/bk1tlcw/?aqi_bid=${bid_no}`);
+bid_no && fetch(`https://hooks.zapier.com/hooks/catch/{{hook_key}}/?aqi_bid=${bid_no}`);
